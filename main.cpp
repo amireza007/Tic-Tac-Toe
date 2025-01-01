399538c21cb9140e7c68a65eaf829a2520b5b35a
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2025-01-01 20:11:20 +0330

    very important comments and TODOs added

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <typeinfo>
 #include <limits>
+#include <stdio.h>
 
 using namespace std;
 
 char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+int gamestate = -2; //-2 means "game isn't finished yet, 0 = draw, 1 = O won, -1 = X won
 
 void printBoard()
 {
@@ -50,27 +52,34 @@ void makeMove(int position, int player)
     int col = (position - 1) % 3;
 
     board[row][col] = symbol;
+   
 }
 
 bool isWinner()
 {
     for (int i = 0; i < 3; i++)
     {
-        if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][2] == board[i][0])
+        if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][2] == board[i][0]) // row win
         {
+            gamestate = -1 ? board[i][0] == 'X' : +1;
             return true;
         }
-        else if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[2][i] == board[0][i])
+        else if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[2][i] == board[0][i]) // column win
         {
+            gamestate = -1 ? board[0][i] == 'X' : +1;
             return true;
         }
     }
 
     if (board[0][0] != ' ' && board[1][1] == board[0][0] && board[2][2] == board[0][0])
+    {
+        gamestate = -1 ? board[0][0] == 'X' : +1;
         return true;
-    else if (board[0][2] != ' ' && board[1][1] == board[0][2] && board[2][0] == board[0][2])
+    }
+    else if (board[0][2] != ' ' && board[1][1] == board[0][2] && board[2][0] == board[0][2]){
+        gamestate = -1 ? board[0][2] == 'X' : +1;
         return true;
-
+    }
     return false;
 }
 
@@ -80,7 +89,6 @@ bool isDraw()
         for (int j = 0; j < 3; j++)
             if (board[i][j] == ' ')
                 return false;
-
     return true;
 }
 
@@ -92,17 +100,50 @@ void printDots(int n)
         system("sleep 0.5");
     }
 }
+// We should have Memoized, we ain't no stupid! => alpha-beta Pruning
+int MiniMax(char nodes[3][3], int depth, bool maximizingPlayer) {
+    if (depth == 0 || gamestate != -2)
+        return gamestate;
+    else if (maximizingPlayer) {
+        int value = -10000000;
+        for (int i; i < depth; i++) { //WTF does Depth mean? Depth should be the number of empty cells!
+            // make a move and update gamestate!
+            value = std::max(value, MiniMax(nodes, depth - 1, false));
+        }
+        return value;
+    }
+    else {
+        int value = +1000000;
+        for (int i; i < depth; i++) {
+            value = std::min(value, MiniMax(nodes, depth - 1, false));
+        }
+        return value;
+    }
+}
+// we don't need alpha-beta pruning. We, Because searching is "Easy", as the heuristic is not good!
+void aiMove() {
+    // making a random move
+    for (int i; i < 9; i++) {
+        int move = 1 + (rand() % 9);
+        if (isValidChoice(move))
+            if (MiniMax(board,9,false) == 1)
+                return;
+    }
+    
+}
 
-void playGame()
+void playGame(int mode)
 {
     int choice = 0;
     int player = 0;
-    int winner = 0;
+    int winner = 0; //if mode is 1, player 2 becomes the Computer
     bool draw = false;
+    if (mode == 1)
+        player = 2; //technically, player 3 is the ai... whatever!
     while (true)
     {
         system("cls");
-        printBoard();
+        printBoard(); 
         if (winner != 0)
         {
             cout << "Player " << winner << " Won! ";
@@ -117,7 +158,10 @@ void playGame()
         }
 
         cout << "Player " << player + 1 << " Turn: ";
-        cin >> choice;
+        if ((player + 1) / 3 == 1 && mode == 1)
+            aiMove();
+        else 
+            cin >> choice;
         if (isValidChoice(choice))
         {
             makeMove(choice, player);
@@ -132,7 +176,6 @@ void playGame()
             cin.clear();
             continue;
         }
-
         if (isWinner())
             winner = player + 1;
         else if (isDraw())
@@ -144,6 +187,7 @@ void playGame()
 
 int main(int argc, char* argv[])
 {
-    playGame();
+    int mode = 1; //1 is for computer vs human
+    playGame(mode);
     return 0;
 }
